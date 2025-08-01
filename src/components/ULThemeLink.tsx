48eb7bd7400dea4ae79525ecfa8de646b5ea1cfb
grandmaester <rohitsharma123902@gmail.com>
2025-08-01 14:24:44 +0530

    feat: Removed React Forward ref, and passed ref as prop to link component

--- src/components/ULThemeLink.tsx
+++ src/components/ULThemeLink.tsx
@@ -1,15 +1,11 @@
 import * as React from "react";
-import { Link as LinkComponent, type LinkProps } from "@/components/ui/link";
+import { Link, type LinkProps } from "@/components/ui/link";
 import { cn } from "@/lib/utils";
 import { extractTokenValue } from "@/utils/helpers/tokenUtils";
 
-const Link = React.forwardRef<HTMLAnchorElement, LinkProps>((props, ref) =>
-  LinkComponent(props, ref)
-);
-
 export interface ULThemeLinkProps extends LinkProps {
   /**
-   * The content of the card element.
+   * The content of the link element.
    */
   children: React.ReactNode;
   /**
@@ -20,46 +16,54 @@ export interface ULThemeLinkProps extends LinkProps {
    * Optional flag to disable the link.
    */
   disabled?: boolean;
+  /**
+   * Optional ref for the link element.
+   */
+  ref?: React.Ref<HTMLAnchorElement>;
 }
 
-const ULThemeLink = React.forwardRef<HTMLAnchorElement, ULThemeLinkProps>(
-  ({ children, className, disabled = false, ...props }, ref) => {
-    // Base component styles
-    const baseStyles =
-      "text-link-focus text-(length:--ul-theme-font-links-size) font-(weight:--ul-theme-font-links-weight) focus:rounded-(--ul-theme-border-links-border-radius) hover:text-link-focus/80";
-    // Disabled state styles
-    const disabledStyles = disabled
-      ? "pointer-events-none text-muted cursor-not-allowed"
-      : "";
-    // UL theme overrrides
-    const variantThemeOverrides =
-      "theme-universal:focus:outline-none theme-universal:focus:ring-4 theme-universal:focus:ring-base-focus/15 theme-universal:focus:bg-base-focus/15"; // focus base color
+const ULThemeLink = ({
+  children,
+  className,
+  disabled = false,
+  ref,
+  ...props
+}: ULThemeLinkProps) => {
+  // Base component styles
+  const baseStyles =
+    "text-link-focus text-(length:--ul-theme-font-links-size) font-(weight:--ul-theme-font-links-weight) focus:rounded-(--ul-theme-border-links-border-radius) hover:text-link-focus/80";
 
-    // Using extractTokenValue utility to extract the link style variant type from the CSS variable
-    const linkStyleValue =
-      extractTokenValue("--ul-theme-font-links-style") === "normal"
-        ? "none"
-        : "always";
+  // Disabled state styles
+  const disabledStyles = disabled
+    ? "pointer-events-none text-muted cursor-not-allowed"
+    : "";
 
-    return (
-      <Link
-        ref={ref}
-        className={cn(
-          baseStyles,
-          disabledStyles,
-          variantThemeOverrides,
-          className
-        )}
-        underline={linkStyleValue}
-        aria-disabled={disabled}
-        {...props}
-      >
-        {children}
-      </Link>
-    );
-  }
-);
+  // UL theme overrides
+  const variantThemeOverrides =
+    "theme-universal:focus:outline-none theme-universal:focus:ring-4 theme-universal:focus:ring-base-focus/15 theme-universal:focus:bg-base-focus/15"; // focus base color
 
-ULThemeLink.displayName = "ULThemeLink";
+  // Using extractTokenValue utility to extract the link style variant type from the CSS variable
+  const linkStyleValue =
+    extractTokenValue("--ul-theme-font-links-style") === "normal"
+      ? "none"
+      : "always";
+
+  return (
+    <Link
+      ref={ref}
+      className={cn(
+        baseStyles,
+        disabledStyles,
+        variantThemeOverrides,
+        className
+      )}
+      underline={linkStyleValue}
+      aria-disabled={disabled}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+};
 
 export default ULThemeLink;
