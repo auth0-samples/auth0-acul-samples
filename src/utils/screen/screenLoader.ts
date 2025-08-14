cce74fc6a1c1a8b6a4c2d30e3b23b9396e573de7
tanejatanya63 <tanejatanya63@gmail.com>
2025-08-14 17:26:32 +0530

    feat: phone identifier challenge screen skeleton

--- src/utils/screen/screenLoader.ts
+++ src/utils/screen/screenLoader.ts
@@ -4,6 +4,9 @@ const SCREEN_COMPONENTS: Record<string, React.ComponentType> = {
   "login-id": lazy(() => import("@/screens/login-id")),
   login: lazy(() => import("@/screens/login")),
   "login-password": lazy(() => import("@/screens/login-password")),
+  "phone-identifier-challenge": lazy(
+    () => import("@/screens/phone-identifier-challenge")
+  ),
 };
 
 export const getScreenComponent = (
