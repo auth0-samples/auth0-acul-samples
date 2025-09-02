578d58c5ccd98b9ae9780d0c3088ae92d3fec008
tanejatanya63 <tanejatanya63@gmail.com>
2025-09-03 03:10:49 +0530

    chore: added error handling

--- react/src/screens/reset-password-email/index.tsx
+++ react/src/screens/reset-password-email/index.tsx
@@ -1,16 +1,14 @@
 import ULThemeCard from "@/components/ULThemeCard";
 import ULThemePageLayout from "@/components/ULThemePageLayout";
-import { ULThemePrimaryButton } from "@/components/ULThemePrimaryButton";
 import { applyAuth0Theme } from "@/utils/theme/themeEngine";
 
 import Header from "./components/Header";
+import ResendEmail from "./components/ResendEmail";
 import { useResetPasswordEmailManager } from "./hooks/useResetPasswordEmailManager";
 
 function ResetPasswordEmailScreen() {
   // Extracting attributes from hook made out of ResetPasswordEmailInstance class of Auth0 React SDK
-  const { resetPasswordEmail, texts, handleResendEmail } =
-    useResetPasswordEmailManager();
-  const buttonText = texts?.resendLinkText || "Resend email";
+  const { resetPasswordEmail, texts } = useResetPasswordEmailManager();
 
   // Apply theme from SDK instance when screen loads
   applyAuth0Theme(resetPasswordEmail);
@@ -21,14 +19,7 @@ function ResetPasswordEmailScreen() {
     <ULThemePageLayout className="theme-universal">
       <ULThemeCard className="w-full max-w-[400px] gap-0">
         <Header />
-        <ULThemePrimaryButton
-          variant="outline"
-          type="button"
-          className="w-full"
-          onClick={handleResendEmail}
-        >
-          {buttonText}
-        </ULThemePrimaryButton>
+        <ResendEmail />
       </ULThemeCard>
     </ULThemePageLayout>
   );
