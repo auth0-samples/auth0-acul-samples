42e4f10b0fecb3cf5f8ec3135d4fa2f9ff759777
Tanya <tanya.taneja@okta.com>
2025-09-22 15:35:14 +0530

    feat: mfa-begin-enroll-options screen

---
+++ src/screens/mfa-begin-enroll-options/index.tsx
@@ -0,0 +1,28 @@
+import ULThemeCard from "@/components/ULThemeCard";
+import ULThemePageLayout from "@/components/ULThemePageLayout";
+import { applyAuth0Theme } from "@/utils/theme/themeEngine";
+
+import Header from "./components/Header";
+import MFAEnrollOptions from "./components/MFAEnrollOptions";
+import { useMfaBeginEnrollOptionsManager } from "./hooks/useMFABeginEnrollOptionsManager";
+
+function MFABeginEnrollOptions() {
+  // Extracting attributes from hook made out of MFABeginEnrollOptionsInstance class of Auth0 React SDK
+  const { mfaBeginEnrollOptions, texts } = useMfaBeginEnrollOptionsManager();
+
+  // Apply theme from SDK instance when screen loads
+  applyAuth0Theme(mfaBeginEnrollOptions);
+  document.title = texts?.pageTitle || "Add another authentication method";
+
+  return (
+    // Applying UDS theme overrides using the "theme-universal" class
+    <ULThemePageLayout className="theme-universal">
+      <ULThemeCard className="w-full max-w-[400px] gap-0">
+        <Header />
+        <MFAEnrollOptions />
+      </ULThemeCard>
+    </ULThemePageLayout>
+  );
+}
+
+export default MFABeginEnrollOptions;
