88cc956e49949abb7901630533524014def67acc
Tanya <tanya.taneja@okta.com>
2025-09-23 12:03:52 +0530

    fix: removed getThemeFromSDK utility

--- src/hooks/useCaptcha.ts
+++ src/hooks/useCaptcha.ts
@@ -1,12 +1,9 @@
 import { useCallback, useState } from "react";
 
 import type { ICaptcha } from "@/components/Captcha/index";
+import { extractTokenValue } from "@/utils/helpers/tokenUtils";
 
-export const useCaptcha = (
-  captchaConfig?: ICaptcha,
-  label?: string,
-  theme?: "light" | "dark" | "auto"
-) => {
+export const useCaptcha = (captchaConfig?: ICaptcha, label?: string) => {
   const isCaptchaEnabled = !!captchaConfig && !!captchaConfig.provider;
 
   const [isValid, setIsValid] = useState<boolean>(!isCaptchaEnabled);
@@ -14,6 +11,16 @@ export const useCaptcha = (
   const [internalError, setInternalError] = useState<string | undefined>(
     undefined
   );
+
+  const getCaptchaTheme = (): "light" | "dark" | "auto" => {
+    const allowedThemes = ["light", "dark", "auto"] as const;
+    const rawTheme = extractTokenValue("--ul-theme-captcha-widget");
+
+    const isValidTheme = (theme: unknown): theme is "light" | "dark" | "auto" =>
+      allowedThemes.includes(theme as "light" | "dark" | "auto");
+
+    return isValidTheme(rawTheme) ? rawTheme : "auto";
+  };
 
   const handleValidationChange = useCallback(
     (valid: boolean, val?: string, err?: string) => {
@@ -30,7 +37,7 @@ export const useCaptcha = (
       label,
       onValidationChange: handleValidationChange,
       error: internalError,
-      theme: theme,
+      theme: getCaptchaTheme(),
     },
     captchaValue: value,
     isCaptchaSolved: isValid,
