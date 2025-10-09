291c2d7148b4eb7f430cf62fe74a875fcbd3596b
Tanya <tanya.taneja@okta.com>
2025-10-09 15:23:41 +0530

    chore: fixed Header.test.tsx file

--- src/screens/mfa-enroll-result/components/Header.tsx
+++ src/screens/mfa-enroll-result/components/Header.tsx
@@ -6,89 +6,98 @@ import { extractTokenValue } from "@/utils/helpers/tokenUtils";
 
 import { useMfaEnrollResultManager } from "../hooks/useMfaEnrollResultManager";
 
+const getTitle = (
+  data: {
+    status: string;
+  } | null,
+  texts: Record<string, string> | null
+) => {
+  switch (data?.status) {
+    case "success":
+      return texts?.enrolledTitle || "You're All Set!";
+    case "already-enrolled":
+      return texts?.alreadyEnrolledTitle || "Already enrolled";
+    case "already-used":
+      return texts?.alreadyUsedTitle || "Already used";
+    case "invalid-ticket":
+      return texts?.invalidTicketTitle || "Invalid Link";
+    case "expired-ticket":
+      return texts?.expiredTicketTitle || "Expired Link";
+    default:
+      return texts?.genericError || "Something Went Wrong";
+  }
+};
+
+const getDescription = (
+  data: {
+    status: string;
+  } | null,
+  texts: Record<string, string> | null
+) => {
+  switch (data?.status) {
+    case "success":
+      return (
+        texts?.enrolledDescription ||
+        "You have successfully added a new authentication factor."
+      );
+    case "already-enrolled":
+      return (
+        texts?.alreadyEnrolledDescription ||
+        "Two-factor Verification has Already Been Enabled."
+      );
+    case "already-used":
+      return (
+        texts?.alreadyUsedDescription ||
+        "This link has already been used. Please get a new link to enroll with Multi-factor Authentication."
+      );
+    case "invalid-ticket":
+      return (
+        texts?.invalidTicketDescription || "This link is invalid or expired."
+      );
+    case "expired-ticket":
+      return texts?.expiredTicketDescription || "This link is expired.";
+    default:
+      return "";
+  }
+};
+
+const logoWidget = (data: { status: string } | null) => {
+  if (data?.status === "success") {
+    return (
+      <CircleCheck
+        data-testid="success-icon"
+        color={themedLogoWidgetColorSuccess}
+        size={80}
+        strokeWidth={1}
+        className="flex flex-wrap justify-widget-logo ml-28"
+      />
+    );
+  } else {
+    return (
+      <CircleX
+        data-testid="error-icon"
+        color={themedLogoWidgetColorError}
+        size={80}
+        strokeWidth={1}
+        className="flex flex-wrap justify-widget-logo ml-28"
+      />
+    );
+  }
+};
+
+const themedLogoWidgetColorSuccess = extractTokenValue(
+  "--ul-theme-color-success"
+);
+const themedLogoWidgetColorError = extractTokenValue("--ul-theme-color-error");
+
 function Header() {
   const { texts, data } = useMfaEnrollResultManager();
-  const themedLogoWidgetColorSuccess = extractTokenValue(
-    "--ul-theme-color-success"
-  );
-  const themedLogoWidgetColorError = extractTokenValue(
-    "--ul-theme-color-error"
-  );
-
-  function getTitle() {
-    switch (data?.status) {
-      case "success":
-        return texts?.enrolledTitle || "You're All Set!";
-      case "already-enrolled":
-        return texts?.alreadyEnrolledTitle || "Already enrolled";
-      case "already-used":
-        return texts?.alreadyUsedTitle || "Already used";
-      case "invalid-ticket":
-        return texts?.invalidTicketTitle || "Invalid Link";
-      case "expired-ticket":
-        return texts?.expiredTicketTitle || "Expired Link";
-      default:
-        return texts?.genericError || "Something Went Wrong";
-    }
-  }
-
-  function getDescription() {
-    switch (data?.status) {
-      case "success":
-        return (
-          texts?.enrolledDescription ||
-          "You have successfully added a new authentication factor."
-        );
-      case "already-enrolled":
-        return (
-          texts?.alreadyEnrolledDescription ||
-          "Two-factor Verification has Already Been Enabled."
-        );
-      case "already-used":
-        return (
-          texts?.alreadyUsedDescription ||
-          "This link has already been used. Please get a new link to enroll with Multi-factor Authentication."
-        );
-      case "invalid-ticket":
-        return (
-          texts?.invalidTicketDescription || "This link is invalid or expired."
-        );
-      case "expired-ticket":
-        return texts?.expiredTicketDescription || "This link is expired.";
-      default:
-        return "";
-    }
-  }
-
-  function logoWidget() {
-    if (data?.status === "success") {
-      return (
-        <CircleCheck
-          data-testid="success-icon"
-          color={themedLogoWidgetColorSuccess}
-          size={80}
-          strokeWidth={1}
-          className="flex flex-wrap justify-widget-logo ml-28"
-        />
-      );
-    } else {
-      return (
-        <CircleX
-          data-testid="error-icon"
-          color={themedLogoWidgetColorError}
-          size={80}
-          strokeWidth={1}
-          className="flex flex-wrap justify-widget-logo ml-28"
-        />
-      );
-    }
-  }
 
   return (
     <>
-      {logoWidget()}
-      <ULThemeTitle>{getTitle()}</ULThemeTitle>
-      <ULThemeSubtitle>{getDescription()}</ULThemeSubtitle>
+      {logoWidget(data)}
+      <ULThemeTitle>{getTitle(data, texts)}</ULThemeTitle>
+      <ULThemeSubtitle>{getDescription(data, texts)}</ULThemeSubtitle>
     </>
   );
 }
